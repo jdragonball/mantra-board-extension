a36e928b73e336cdfa32c294fa0a9a28486188a9
jdragonball <jaeyong.sinn@gmail.com>
2020-12-30 17:34:30 +0900

    local storage 1차 구현

---
+++ js/index.js
@@ -0,0 +1,39 @@
+function init() {
+  const MANTRA_KEY = "MANTRA";
+
+  const mantraBox = document.querySelector(".mantra-box");
+
+  // 기존 만트라 불러오기
+  const mantra = localStorage.getItem(MANTRA_KEY);
+  // 이미 있으면 box에 보여주기
+  if (mantra && mantra.trim()) {
+    mantraBox.innerHTML = mantra;
+  }
+
+  
+  mantraBox.addEventListener("click", (e) => {
+    mantraBox.setAttribute("contenteditable", true);
+    mantraBox.focus();
+  });
+
+  // 엔터키 입력
+  mantraBox.addEventListener("keypress", (e) => {
+    if (e.keyCode == 13) {
+      if (!e.shiftKey) {
+        e.preventDefault();
+        if (!mantraBox.textContent.trim()) {
+          mantraBox.innerHTML = "";
+        }
+        mantraBox.dispatchEvent(new Event("focusout"));
+        mantraBox.setAttribute("contenteditable", false);
+      }
+    }
+  });
+
+  // 새 만트라 입력후 포커스 아웃
+  mantraBox.addEventListener("focusout", (e) => {
+    localStorage.setItem(MANTRA_KEY, mantraBox.innerHTML);
+  });
+};
+
+init();
